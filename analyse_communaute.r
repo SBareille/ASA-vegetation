56bf7c7b2ca7127bfb33c87480a282dd2582fccf
SBareille <servanebareille@hotmail.fr>
2020-10-18 12:46:49 +0200

    Analyses flo et mil au début

--- analyse_communaute.r
+++ analyse_communaute.r
@@ -3,17 +3,43 @@ library(vegan)
 library(ggplot2)
 library(tidyr)
 library(dplyr)
-  
-setwd("~/Etudes/Rennes1/S3/ASA/Project")
+
+setwd("C:/Users/serva/Google Drive/1-Partage Ordis/M2/ASA/Projet")
 
 assoc <- read.table("association.txt", header = TRUE)
 coord <- read.table("coord.txt", header = TRUE)
 flo <- read.table("flo.txt",header = TRUE)
 mil <- read.table("mil.txt",header = TRUE)
 
+#################### flo INSPECTION ############################################
+
+names(flo)
+str(flo)
+effectifs=apply(X = flo, 2, sum)
+
+par(mfrow=c(1,1))
+hist(effectifs
+     ,breaks=20
+     , col='red'
+     , xlab='Species effectif'
+     , ylab='Number of Species')
 
 
-### AFC sur le tableau des occurences d'espèces ----
+#################### mil INSPECTION ############################################
+
+env_data <- mil[-10] %>% 
+  pivot_longer(cols = 1:10,  names_to = "Variable", values_to = "Y" ) %>%
+  arrange(Variable)
+
+
+ggplot(env_data, aes(x = Variable, y = Y, colour = Variable))+
+  geom_point(position = position_jitterdodge(dodge.width = 0.7),size = 2)+
+  geom_boxplot(alpha = 0.5)
+
+
+
+
+#################### AFC sur le tableau des occurences d'espèces#################### 
 
 afc_flo <- dudi.coa(flo, scannf = FALSE, nf = 2)
 summary(afc_flo)
@@ -23,10 +49,9 @@ s.label(afc_flo$co)
 
 iner_afc_flo <- inertia.dudi(afc_flo, col.inertia = TRUE, row.inertia = TRUE)
 a = format(iner_afc_flo$col.abs,scientific = FALSE)
-seuil <- 1/56
-seuil <- seuil*100
+seuil_AFC<-1/min(c(nrow(flo),ncol(AFC_flo)))*100
 
-which(iner_afc_flo$col.abs[,1] > seuil)
+which(iner_afc_flo$col.abs[,1] > seuil_AFC)
 
 dist_flo <- dist.quant(afc_flo$li,1)
 
@@ -49,17 +74,4 @@ s.label(afc_flo$co[indice_axe1,], boxes = FALSE)
 
 indice_axe2 <- which(iner_afc_flo$col.abs[,2] > seuil)
 s.label(afc_flo$co[indice_axe2,], boxes=TRUE, add.plot = TRUE)
-
- 
-
-### Analyse des données environnementales ----
-env_data <- mil[-10] %>% 
-  pivot_longer(cols = 1:10,  names_to = "Variable", values_to = "Y" ) %>%
-  arrange(Variable)
-
-
-ggplot(env_data, aes(x = Variable, y = Y, colour = Variable))+
-  geom_point(position = position_jitterdodge(dodge.width = 0.7),size = 2)+
-  geom_boxplot(alpha = 0.5)
-
 
