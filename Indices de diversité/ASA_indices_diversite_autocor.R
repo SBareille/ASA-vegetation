803ac38678aa24a12c8f47ab80847db2450d73ed
KaRMaX-aka-flo <72971497+KaRMaX-aka-flo@users.noreply.github.com>
2020-10-23 18:58:28 +0200

    Update ASA_indices_diversite_autocor.R

--- Indices de diversité/ASA_indices_diversite_autocor.R
+++ Indices de diversité/ASA_indices_diversite_autocor.R
@@ -1,11 +1,3 @@
-coord <- read.table("coord.txt", header = TRUE)
-flo <- read.table("flo.txt",header = TRUE)
-mil <- read.table("mil.txt",header = TRUE)
-
-
-
-
-
 library(ade4)
 library(spdep)
 library(tripack)
@@ -18,22 +10,22 @@ install_version("TSA", version = "1.2.1", repos = "http://cran.us.r-project.org"
 install_version("TSA", version = "1.2.1", repos = "https://pbil.univ-lyon1.fr/CRAN/")
 library(TSA) 
 
+coord <- read.table("coord.txt", header = TRUE)
+flo <- read.table("flo.txt",header = TRUE)
+mil <- read.table("mil.txt",header = TRUE)
 
 
 data=cbind(flo,mil,coord)
-plot(data$x,data$y) #representation des coordonnees des stations
-xy=data[,68:69] #extraction des coordonnees (x et y)
-knear4<-knearneigh(as.matrix(xy),4) # estimation des connexions voisines de type tour par le critere des 4 plus proches voisins
+plot(data$x,data$y) #representation of station coordinates
+xy=data[,68:69] #extraction of the coordinates
+knear4<-knearneigh(as.matrix(xy),4) # rook type of connection approximate by the 4th nearest neighbor criteria
 knear4
 knn2nb(knear4)
 
 
+space_mat.bin<- nb2listw(knn2nb(knear4), style="B",zero.policy=TRUE) # spatial weighted matrix with binary weight formulation
 
-
-space_mat.bin<- nb2listw(knn2nb(knear4), style="B",zero.policy=TRUE) # matrice des poids spatiale avec la formule des poids binaire
-#space_mat.stan<- nb2listw(knn2nb(knear4), style="W", zero.policy=TRUE)
-
-# Calcul de l'indice de diversite de Simpson
+# Calculation if Simpson Index
 Simpson=function(l){
   N=sum(l)
   Si = 0
@@ -44,22 +36,19 @@ Simpson=function(l){
   return(1-Si/(N*(N-1)))
 }
 
-SimFlo=apply(flo, 1, Simpson) # calcul de l'indice de Simpson pour les especes du jeu de donnes
+SimFlo=apply(flo, 1, Simpson) # Calculation of Simpson Index for plant species
 data=cbind(data,SimFlo)
 
 
 
-# test de Moran sur l'indice de Simpson
+# Moran test on Simpson Index
 moran.test(data[,70] , space_mat.bin)
 
 
-#autocorrelogramme de l'indice de Simpson
+#autocorrelogramm of Simson Index
 cor4_flo<-sp.correlogram(knn2nb(knear4), data[,70], order=10, method="I",zero.policy=TRUE, style="B")
 print(cor4_flo,p.adjust.method="bonferonni")
-plot(cor4_flo,main="Moran autocorrelogram for Simpson index (4 neighbors criterion)")
-
-#c'est bon 
-
+plot(cor4_flo,main="Moran autocorrelogram for Simpson Index (4 neighbors criterion)")
 
 
 #calculation of Shannon index
@@ -77,7 +66,8 @@ Shannon=function(l){
 
 sh_flo=Shannon(flo)
 
-Pielou=function(f){#calcul de l'indice de Pielou
+#calculation of Pielou Index
+Pielou=function(f){
   H=Shannon(f)
   I=rep(0,length(H))
   for(i in 1:length(H)){
@@ -90,34 +80,9 @@ P=Pielou(flo)
 
 data=cbind(data,P)
 
-# test de Moran sur l'indice de Shannon
+# Moran test on Pielou Index
 moran.test(data[,70] , space_mat.bin)
 
 cor4_piel<-sp.correlogram(knn2nb(knear4), data[,71], order=10, method="I",zero.policy=TRUE, style="B")
 print(cor4_piel,p.adjust.method="bonferonni")
-plot(cor4_piel,main="Moran autocorrelogram for Pielou idex (4 neighbors criterion)")
-
-
-
-
-
-################################################
-##BROUILLON, TENTATIVES DE TRUCS
-################################################
-
-
-moddddd=lm(association~Altitude+Argile+Sable+K2O+Conduc+SimFlo+Mg..+Capa_Reten+Na..l, data = div35_2)
-
-
-
-moddd=lm(SimFlo~association, data = div35_2)
-
-##test de Moran sur les associations##
-#PAS POSSIBLE
-assoc <- read.table("association.txt", header = TRUE)
-data=cbind(data,assoc)
-moran.test(data[,71] , space_mat.bin)
-
-cor4_asso<-sp.correlogram(knn2nb(knear4), div35_2[,71], order=10, method="I",zero.policy=TRUE, style="B")
-print(cor4_asso,p.adjust.method="bonferonni")
-plot(cor4_asso,main="Moran autocorrelogram for Simpson index on associations (4 neighbors criterion)")
+plot(cor4_piel,main="Moran autocorrelogram for Pielou Index (4 neighbors criterion)")
