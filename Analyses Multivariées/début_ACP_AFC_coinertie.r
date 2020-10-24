23ef083ca051420c6961e2a23ee8ada1e7007a0b
SBareille <72937199+SBareille@users.noreply.github.com>
2020-10-24 11:45:34 +0200

    mise en forme
    - english comments
    - bonne orga (ca avant pca) 
    - centrer réduire et hellinger
    -couleurs graphs
    - ATTENTION, il reste coinertie

--- Analyses Multivariées/début_ACP_AFC_coinertie.r
+++ Analyses Multivariées/début_ACP_AFC_coinertie.r
@@ -1,6 +1,7 @@
 library(ade4)
 library(vegan)
 library(ggplot2)
+library(RColorBrewer)
 library(corrplot)
 library(tidyr)
 library(dplyr)
@@ -10,122 +11,132 @@ setwd("C:/Users/serva/Google Drive/1-Partage Ordis/M2/ASA/Projet")
 assoc <- read.table("association.txt", header = TRUE)
 coord <- read.table("coord.txt", header = TRUE)
 flo <- read.table("flo.txt",header = TRUE)
-mil <- read.table("mil.txt",header = TRUE)
+env <- read.table("mil.txt",header = TRUE)
 
-############## ACP sur les variabels environnementales #########
-# Matrice des corrÃ©lations
-mcor <- cor(mil)
+# standardization of data before statistical analysis
+flo <- decostand(flo, "hellinger") # Lots of zero
+env= as.data.frame(scale(env, center=TRUE, scale=TRUE)) # variables with different units
+
+#################### CA on the Species occurrence table (with stations, and then associations) #################### 
+
+# We have already used the hellinger transformation on the data
+
+CA_flo <- dudi.coa(flo, scannf = FALSE, nf = 2) # we keep two axis
+summary(CA_flo)
+CA_flo$eig # Gross eigen values of axis
+CA_pourcent <- CA_flo$eig*100/sum(CA_flo$eig) # Pourcentages of eigen values of axis
+barplot(CA_pourcent, main = "Histogram of Pourcentages of eigen values", ylim=c(0,50), col = heat.colors(10))
+s.label(CA_flo$li) # Stations display
+s.label(CA_flo$co) # Species display
+
+iner_CA_flo <- inertia.dudi(CA_flo, col.inertia = TRUE, row.inertia = TRUE)
+iner_CA_flo$col.abs # Absolute contribution of species on axis
+
+threshold_CA<-1/min(c(nrow(flo),ncol(CA_flo)))*100 # ordinarily used for CA
+threshold_CA
+
+row.names(iner_CA_flo$col.abs)[which(iner_CA_flo$col.abs[,1] > threshold_CA)] # Species which contribute the most to axis 1
+row.names(iner_CA_flo$col.abs)[which(iner_CA_flo$col.abs[,2] > threshold_CA)] # Species which contribute the most to axis 2
+
+# dist_flo <- dist.quant(CA_flo$li,1)
+# 
+# dendo <- hclust(dist_flo,"ward.D2")
+# 
+# plot(dendo$height, type="s")
+# abline(h = 1.94)
+# 
+# plot(dendo)
+# abline(h = 1.94)
+# 
+# decoup <- cutree(dendo, 7)
+# decoup
+# 
+# s.class(CA_flo$li,as.factor(decoup))
+# 
+# # Affichage des Ã©tiquettes qui correspondent 
+# indice_axe1 <- which(iner_CA_flo$col.abs[,1] > threshold)
+# s.label(CA_flo$co[indice_axe1,], boxes = FALSE)
+# 
+# indice_axe2 <- which(iner_CA_flo$col.abs[,2] > threshold)
+# s.label(CA_flo$co[indice_axe2,], boxes=TRUE, add.plot = TRUE)
+
+#  we want to see vegetal associations on the graph to study the species repartition on them
+asso <- assoc %>%
+  mutate(
+    group = dense_rank(association) # creates a column : if station is "C1", then ots rank will be "1"
+  ) %>%
+  select(group)
+decoup_asso <- (t(asso)) # ligns become colomns and colomns become ligns
+
+#s.label(CA_flo$li) # station with CA
+s.class(CA_flo$li,as.factor(decoup_asso), col=brewer.pal(n=7, name = 'Set2'), label = c("","","","","","",""), add.plot=T) # add group of associations.
+title(main = "CA with association groups") # add title
+legend(x= 1,y = 1,legend = c("C1","C2","C3","C4","C5","C6","C7"),col=brewer.pal(n=7, name = 'Set2'), pch = 1) # add legend
+
+############## PCA on environmental variables (with stations, and then associations) #########
+
+# We already have centered and reduce the data
+
+# Correlation matrix
+mcor <- cor(env)
 corrplot(mcor, method = "number", type = "upper", order = "hclust", tl.col = "black")
+# Na+/L and Conductivity are highly correlated (0.96). Na+/L is also correlated with NA+/100g (0.71), as we may have guessed 
+# We decide to get rid of Na+/L
+data_PCA <- env[,-10]
+View(data_PCA)
 
-data_ACP <- mil[,-10]
-View(data_ACP)
+# PCA
+PCA_env <- dudi.pca(data_PCA, scannf = FALSE, nf = 2) # we keep 2 axis
+PCA_env$eig # Gross eigen values of axis
+PCA_pourcent <- PCA_env$eig*100/sum(PCA_env$eig) # Pourcentages of eigen values of axis
+barplot(PCA_pourcent, main = "Histogram of Pourcentages of eigen values", ylim=c(0,50), col = heat.colors(10))
+s.label(PCA_env$li, boxes = FALSE) # Stations display
+s.corcircle(PCA_env$co) # Environmental variables display
 
-# RÃ©alisation de l'ACP
-acp_env <- dudi.pca(data_ACP, scannf = FALSE, nf = 2)
-acp_env$eig # Valeurs propres brutes 
-acp_env$eig*100/sum(acp_env$eig) # Valeurs propres en pourcentage
-s.label(acp_env$li, boxes = FALSE) # Affichage des stations dans le nouveau plan formÃ© par l'acp
-s.corcircle(acp_env$co) ## Affichage des variables environnementales dans le nouveau plan de l'acp
+iner_PCA_env <- inertia.dudi(PCA_env, col.inertia = TRUE, row.inertia = TRUE)
+iner_PCA_env$col.abs # Absolute contribution of environmental variables on axis
 
-iner_acp_env <- inertia.dudi(acp_env, col.inertia = TRUE, row.inertia = TRUE)
-iner_acp_env$col.abs # RÃ©cupÃ©ration des contributions absolues pour les colonnes (ie les variables environnementales)
+threshold_PCA = 1/length(iner_PCA_env$col.abs[,1])*100
+threshold_PCA
 
-seuil = 1/length(iner_acp_env$col.abs[,1])*100
-seuil
-
-row.names(iner_acp_env$col.abs)[which(iner_acp_env$col.abs[,1] > seuil)] # RÃ©cupÃ©ration des variables qui contribuent Ã  l'axe 1
-row.names(iner_acp_env$col.abs)[which(iner_acp_env$col.abs[,2] > seuil)] # RÃ©cupÃ©ration des variables qui contribuent Ã  l'axe 2
+row.names(iner_PCA_env$col.abs)[which(iner_PCA_env$col.abs[,1] > threshold_PCA)] # Environmental variables which contributes the most to axis 1
+row.names(iner_PCA_env$col.abs)[which(iner_PCA_env$col.abs[,2] > threshold_PCA)] # Environmental variables which contributes the most to axis 2
 
 
-# RÃ©alisation de la CAH sur l'ACP
-dist_env <- dist.quant(acp_env$li,1)
-dendo <- hclust(dist_env,"ward.D2")
-plot(dendo$height, type="s")
-abline(h = 6.7)
+# RÃ©alisation de la CAH sur l'PCA     (j'ai tout mis en commentaire temps qu'on n'a pas décidé ce qu'on en faisait)
+# dist_env <- dist.quant(PCA_env$li,1)
+# dendo <- hclust(dist_env,"ward.D2")
+# plot(dendo$height, type="s")
+# abline(h = 6.7)
+# 
+# plot(dendo)
+# abline(h = 6.7)
+# 
+# decoup <- cutree(dendo, 6)
+# decoup
+# 
+# s.class(PCA_env$li,as.factor(decoup), clabel = 0.5) # Remarque : pour le cluster 6, seulement prÃ©sence de deux stations donc pas bien visible sous l'Ã©tiquette. Changer le clabel Ã  0 pour pouvoir voir les points.
+# 
+# # Affichage des Ã©tiquettes qui correspondent 
+# indice_axe1 <- which(iner_PCA_env$col.abs[,1] > threshold_PCA)
+# s.label(PCA_env$co[indice_axe1,], boxes = FALSE, add.plot = TRUE)
+# 
+# indice_axe2 <- which(iner_PCA_env$col.abs[,2] > threshold_PCA)
+# s.label(PCA_env$co[indice_axe2,], boxes=TRUE, add.plot = TRUE, clabel = 0.8)
 
-plot(dendo)
-abline(h = 6.7)
+#  we want to see see vegetal associations on the graph to study the effect of environmental variables on them
 
-decoup <- cutree(dendo, 6)
-decoup
-
-s.class(acp_env$li,as.factor(decoup), clabel = 0.5) # Remarque : pour le cluster 6, seulement prÃ©sence de deux stations donc pas bien visible sous l'Ã©tiquette. Changer le clabel Ã  0 pour pouvoir voir les points.
-
-# Affichage des Ã©tiquettes qui correspondent 
-indice_axe1 <- which(iner_acp_env$col.abs[,1] > seuil)
-s.label(acp_env$co[indice_axe1,], boxes = FALSE, add.plot = TRUE)
-
-indice_axe2 <- which(iner_acp_env$col.abs[,2] > seuil)
-s.label(acp_env$co[indice_axe2,], boxes=TRUE, add.plot = TRUE, clabel = 0.8)
-
-#  decoupe des stations en fonction des associations #
-asso <- assoc %>%
-  mutate(
-    group = dense_rank(association) # crée une colonne si la station "C1" alors donne le rang "1"
-  ) %>%
-  select(group)
-decoup_asso <- (t(asso)) # passe les lignes deviennent colonnes et colonne deviennent lignes
-s.label(acp_env$li) # Station with CAP
-s.class(acp_env$li, as.factor(decoup_asso), col = 1:7, label = c("","","","","","","") , add.plot = TRUE) # add group of associations
-title(main = "CAP with association group") # add title
-legend(x= 4.5,y = 4,legend = c("C1","C2","C3","C4","C5","C6","C7"),col = 1:7, pch = 1) # add legend
+s.label(PCA_env$li, ) # station with PCA
+s.class(PCA_env$li, as.factor(decoup_asso), col=brewer.pal(n=7, name = 'Set2'), label = c("","","","","","","") , add.plot = TRUE) # add group of associations. decoup_asso has been created for CA
+title(main = "PCA with association group") # add title
+legend(x= 4.5,y = -0.5,legend = c("C1","C2","C3","C4","C5","C6","C7"),col = brewer.pal(n=7, name = 'Set2'), pch = 1) # add legend
 
 
 
-
-#################### AFC sur le tableau des occurences d'espÃ¨ces#################### 
-
-afc_flo <- dudi.coa(flo, scannf = FALSE, nf = 2)
-summary(afc_flo)
-
-s.label(afc_flo$li)
-s.label(afc_flo$co)
-
-iner_afc_flo <- inertia.dudi(afc_flo, col.inertia = TRUE, row.inertia = TRUE)
-a = format(iner_afc_flo$col.abs,scientific = FALSE)
-seuil_AFC<-1/min(c(nrow(flo),ncol(afc_flo)))*100
-
-which(iner_afc_flo$col.abs[,1] > seuil_AFC)
-
-dist_flo <- dist.quant(afc_flo$li,1)
-
-dendo <- hclust(dist_flo,"ward.D2")
-
-plot(dendo$height, type="s")
-abline(h = 1.94)
-
-plot(dendo)
-abline(h = 1.94)
-
-decoup <- cutree(dendo, 7)
-decoup
-
-s.class(afc_flo$li,as.factor(decoup))
-
-# Affichage des Ã©tiquettes qui correspondent 
-indice_axe1 <- which(iner_afc_flo$col.abs[,1] > seuil)
-s.label(afc_flo$co[indice_axe1,], boxes = FALSE)
-
-indice_axe2 <- which(iner_afc_flo$col.abs[,2] > seuil)
-s.label(afc_flo$co[indice_axe2,], boxes=TRUE, add.plot = TRUE)
-
-#  decoupe des stations en fonction des associations #
-asso <- assoc %>%
-  mutate(
-    group = dense_rank(association) # crée une colonne si la station "C1" alors donne le rang "1"
-  ) %>%
-  select(group)
-decoup_asso <- (t(asso)) # passe les lignes deviennent colonnes et colonne deviennent lignes
-
-s.label(afc_flo$li) # station with AFC
-s.class(afc_flo$li,as.factor(decoup_asso), col = 1:7, label = c("","","","","","",""), add.plot=T) # add group of association
-title(main = "CA with association group") # add title
-legend(x= 1,y = 1,legend = c("C1","C2","C3","C4","C5","C6","C7"),col = 1:7, pch = 1) # add legend
-
-#############coinertia####################
-afc_flo #already done before
-acp_coinertia_env <- dudi.pca(data_ACP, row.w = afc_flo$lw, scannf = FALSE, nf = 2) #weight the PCA row with the CA row weights
-coi <- coinertia(acp_coinertia_env, afc_flo, scannf = FALSE, nf=2) #selection of 2 axis
+#############  coinertia  ####################
+CA_flo #already done before
+PCA_coinertia_env <- dudi.pca(data_PCA, row.w = CA_flo$lw, scannf = FALSE, nf = 2) #weight the PCA row with the CA row weights
+coi <- coinertia(PCA_coinertia_env, CA_flo, scannf = FALSE, nf=2) #selection of 2 axis
 coi
 
 randtest(coi,nrepet=1000)
